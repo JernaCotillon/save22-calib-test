6a0a4b78b66e281f905470f53a2957a7c279d449
jernacotillon <cotillonjerna@gmail.com>
2015-12-08 19:26:18 -0500

    delete

--- exercise1Updated.py
+++
@@ -1,5 +0,0 @@
-first_names = ['Shan', 'Dao Ming', 'Hua Ze', 'Xi', 'Mei']
-last_names = ['Cai', 'Si', 'Lei', 'Men', 'Zuo']
-
-names = [('{} {}'.format(fName,last_names[lName]))  for lName, fName in enumerate(first_names)]
-print (names)
