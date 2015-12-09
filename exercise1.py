ce33cf697a7dc07e04ea30bd9d0cbe5f854797c6
jernacotillon <cotillonjerna@gmail.com>
2015-12-08 19:20:32 -0500

    Updated exercise 1. Join first name and last name. List comprehension.

--- exercise1.py
+++ exercise1.py
@@ -1,5 +1,5 @@
 first_names = ['Shan', 'Dao Ming', 'Hua Ze', 'Xi', 'Mei']
 last_names = ['Cai', 'Si', 'Lei', 'Men', 'Zuo']
 
-names =map(lambda first, last: first + " "+last, first_names, last_names)
-print names
+names = [('{} {}'.format(fName,last_names[lName]))  for lName, fName in enumerate(first_names)]
+print (names)
