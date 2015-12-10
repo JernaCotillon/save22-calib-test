513dd54321c08b940228cdd08ff708944564f0b7
jernacotillon <cotillonjerna@gmail.com>
2015-12-10 00:55:28 -0500

    Use pensize and color

--- turtle.py
+++ turtle.py
@@ -1,23 +1,50 @@
 import turtle
+
+turtle.delay(20)
+turtle.color("blue")
+turtle.pensize(20)
+
 turtle.right(90)
 turtle.forward(100)
+
+turtle.color("red")
+turtle.backward(100)
+turtle.forward(100)
+
+turtle.color("blue")
 turtle.left(90)
 turtle.forward(100)
+
+turtle.color("red")
+turtle.backward(100)
+turtle.forward(100)
+
 
 turtle.penup()
 turtle.left(90)
 turtle.forward(50)
 
+
+turtle.color("blue")
 turtle.pendown()
 turtle.left(90)
 turtle.forward(100)
 
+turtle.color("red")
+turtle.backward(100)
+turtle.forward(100)
+
+turtle.color("blue")
 turtle.penup()
 turtle.backward(100)
 
 turtle.sety(2)
 
 turtle.pendown()
+turtle.forward(100)
+
+turtle.color("red")
+turtle.backward(100)
 turtle.forward(100)
 
 
