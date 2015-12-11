21965ffce544d542eab0317cc2d161a035e75411
jernacotillon <cotillonjerna@gmail.com>
2015-12-11 02:29:07 -0500

    Test Calculator

--- calculator/test.py
+++ calculator/test.py
@@ -3,8 +3,24 @@ import unittest
 
 
 class TestCalculator(unittest.TestCase):
-	def test_Add(self):
-		self.assertEqual(calculato2.add(1,1),2)
+	def test_add(self):
+		self.assertEqual(calculator2.add(1,1),2)
+	
+		with self.assertRaises(TypoError):
+			calculator2.add('1',1)
+
+
+	def test_sub(self):
+		self.assertEqual(calculator2.sub(1,1),0)
+
+	def test_mul(self):
+		self.assertEqual(calculator2.mul(1,1),1)
+
+	def test_div(self):
+		self.assertEqual(calculator2.div(1,1),1)
+		with self.assertRaises(ZeroDivisionError):
+			calculator2.div(10,0)
+
 
 if __name__ == "__main__":
 	unittest.main()
