f1763ee3996d201af39f58cc0236a9bf5ad6bbd2
jernacotillon <cotillonjerna@gmail.com>
2015-12-11 02:39:52 -0500

    Unit testing operator

--- calculator/test.py
+++ calculator/test.py
@@ -9,7 +9,6 @@ class TestCalculator(unittest.TestCase):
 		with self.assertRaises(TypoError):
 			calculator2.add('1',1)
 
-
 	def test_sub(self):
 		self.assertEqual(calculator2.sub(1,1),0)
 
@@ -20,6 +19,13 @@ class TestCalculator(unittest.TestCase):
 		self.assertEqual(calculator2.div(1,1),1)
 		with self.assertRaises(ZeroDivisionError):
 			calculator2.div(10,0)
+
+	def test_operator(self):
+		self.assertEqual(calculator2.add(13,11),24)
+		self.assertEqual(calculator2.sub(15,-10),25)
+		self.assertEqual(calculator2.mul(7,-3),-21)
+		self.assertEqual(calculator2.div(16,2),8)
+
 
 
 if __name__ == "__main__":
